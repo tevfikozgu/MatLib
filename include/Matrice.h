b0c21d84ca34c21ee74f4e32722f7af9e6dffc5d
tevfikozgu <43846480+tevfikozgu@users.noreply.github.com>
2022-03-04 00:26:55 +0300

    Printing Matrice is implemented by overloading << operator.

--- include/Matrice.h
+++ include/Matrice.h
@@ -5,6 +5,7 @@
 #ifndef MATRICE_LIBRARY
 #define MATRICE_LIBRARY
 
+
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -13,18 +14,34 @@
 
 using namespace std;
 
-template <typename T>
-class Matrice
-{
+template<typename T>
+class Matrice {
 private:
     vector<vector<T> > array;
-    int r,c;
+    int r, c;
 public:
     Matrice(int, int);
 
     void init(T arr[]);
-    Matrice<T> operator*(const Matrice&);
-    void row_col_mul(Matrice<T>&, Matrice<T>,int, int);
+
+    Matrice<T> operator*(const Matrice &);
+
+    void row_col_mul(Matrice<T> &, Matrice<T>, int, int);
+
+    friend ostream & operator << (ostream &out, const Matrice<T>& Mat)
+    {
+        for (int i=0;i<Mat.r;i++)
+        {
+            for(int j=0;j<Mat.c;j++)
+            {
+                out << Mat.array[i][j] << "\t";
+            }
+            out << endl;
+        }
+        return out;
+    }
+
 };
+
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,9 @@ int main() {
 
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds = end-start;
-    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    std::cout << "Elapsed Time of Multiplication: " << elapsed_seconds.count() << "s\n";
+
+    cout << Matrice_3;
 
     return 0;
 }
