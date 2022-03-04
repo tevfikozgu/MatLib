72c66de98d4590e3999abe26e10ddf27e9793895
tevfikozgu <43846480+tevfikozgu@users.noreply.github.com>
2022-03-05 00:22:01 +0300

    Code style update is done.

--- libs/MatriceLibrary/include/Matrice.h
+++ libs/MatriceLibrary/include/Matrice.h
@@ -1,8 +1,8 @@
-/**
+/*******************
  * @brief   : This file contains Class structure of MatLib
  * @author  : Tevfik Özgü
  * @date    : 03.03.2022
- */
+ *******************/
 
 
 #ifndef MATRICE_LIBRARY
@@ -19,12 +19,15 @@ using namespace std;
 template<typename T>
 class Matrice {
 private:
-    vector<vector<T> > array;
+    vector<vector<T>> array;
     int r, c;
 public:
+
     Matrice();
     Matrice(int, int);
     Matrice(const Matrice<T>&);
+
+    ~Matrice(){};
 
     void init(T arr[]);
 
@@ -32,14 +35,14 @@ public:
     Matrice<T> operator+(const Matrice &);
     Matrice<T> operator-(const Matrice &);
 
-    static Matrice<T> eye(int);
-
     void row_col_mul(Matrice<T>&, Matrice<T>, int, int);
     void row_row_sum(Matrice<T>&, Matrice<T>, int);
     void row_row_diff(Matrice<T>&, Matrice<T>, int);
 
-    void row_T(Matrice<T>&, int);
     Matrice<T> t();
+    void row_T(Matrice<T>&, int);
+
+    static Matrice<T> eye(int);
 
     friend ostream & operator << (ostream &out, const Matrice<T>& Mat)
     {
@@ -53,6 +56,7 @@ public:
         }
         return out;
     }
+
 };
 
 
